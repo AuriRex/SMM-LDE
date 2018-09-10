5bbb2626a077bd88ff034cfdd5fbfe4d38b248bd
AuriRex <xaurirexx@gmail.com>
2018-09-10 19:55:14 +0200

    mmdrag added

--- src/me/auri/core/LvlViewer.java
+++ src/me/auri/core/LvlViewer.java
@@ -2,15 +2,21 @@ package me.auri.core;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.JViewport;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
 
 public class LvlViewer implements ActionListener {
 
@@ -73,9 +79,52 @@ public class LvlViewer implements ActionListener {
 	            ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 	            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		
+		panel.setAutoscrolls(true);
+
 		scrollPane.getVerticalScrollBar().setUnitIncrement(16);
-		scrollPane.getHorizontalScrollBar().setUnitIncrement(32);
+		scrollPane.getHorizontalScrollBar().setUnitIncrement(16);
 		
+		
+
+		MouseAdapter ma = new MouseAdapter() {
+
+			private Point origin;
+			private boolean mb2 = false;
+			@Override
+			public void mousePressed(MouseEvent e) {
+				origin = new Point(e.getPoint());
+				if(e.getButton() == MouseEvent.BUTTON2)
+					mb2 = true;
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if(e.getButton() == MouseEvent.BUTTON2)
+					mb2 = false;
+			}
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				if(mb2)
+					if (origin != null) {
+						JViewport viewPort = (JViewport) SwingUtilities.getAncestorOfClass(JViewport.class, panel);
+						if (viewPort != null) {
+							int deltaX = origin.x - e.getX();
+							int deltaY = origin.y - e.getY();
+
+							Rectangle view = viewPort.getViewRect();
+							view.x += deltaX;
+							view.y += deltaY;
+
+							panel.scrollRectToVisible(view);
+						}
+					}
+			}
+
+		};
+
+		panel.addMouseListener(ma);
+		panel.addMouseMotionListener(ma);
 		
 		main.add(scrollPane, BorderLayout.CENTER);
 		main.add(left, BorderLayout.WEST);
@@ -123,7 +172,5 @@ public class LvlViewer implements ActionListener {
 			
 		}
 	}
-	
-	
 	
 }
