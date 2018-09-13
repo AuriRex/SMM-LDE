e5c3e4d8c1ddc116feeca08b01ed7a9f0daf2425
AuriRex <xaurirexx@gmail.com>
2018-09-13 22:50:10 +0200

    removed unused import

--- src/me/auri/core/Window.java
+++ src/me/auri/core/Window.java
@@ -14,7 +14,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyVetoException;
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
